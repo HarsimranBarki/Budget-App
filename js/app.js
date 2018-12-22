7b405b109acd96aeff190ea5bd814e121c9cf33b
Harsimran <harsimransinghbarki@gmail.com>
2018-12-22 20:18:02 +0530

    Added New Methods

--- js/app.js
+++ js/app.js
@@ -31,6 +31,27 @@ class UI {
       this.showBalance();
     }
   }
+  // Show Balance
+  showBalance() {
+    const expense = this.totalExpense();
+    const total = parseInt(this.budgetAmount.textContent) - expense;
+    this.balanceAmount.textContent = total;
+    if (total < 0) {
+      this.balance.classList.remove('showGreen', 'showBlack');
+      this.balance.classList.add('showRed');
+    } else if (total > 0) {
+      this.balance.classList.remove('showRed', 'showBlack');
+      this.balance.classList.add('showGreen');
+    } else if (total === 0) {
+      this.balance.classList.remove('showRed', 'showGreen');
+      this.balance.classList.add('showBlack');
+    }
+  }
+  // Total Expense
+  totalExpense() {
+    let total = 400;
+    return total;
+  }
 }
 
 function eventListenters() {
